ee580c3c2588fcde291f493e9d5c4ebcf3b56279
ammar <150704453+ammarnazmi@users.noreply.github.com>
2026-02-09 12:39:30 +0800

    duplicate table (#5)

--- resources/views/admin/authors/index.blade.php
+++ resources/views/admin/authors/index.blade.php
@@ -52,6 +52,45 @@
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>{{ __('ID') }}</th>
+                            <th>{{ __('Name') }}</th>
+                            <th>{{ __('Biography') }}</th>
+                            <th>{{ __('Birth Date') }}</th>
+                            <th width="10%">{{ __('Actions') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <template x-for="author in authors" :key="author.id">
+                            <tr>
+                                <td x-text="author.id"></td>
+                                <td x-text="author.name"></td>
+                                <td x-text="author.bio"></td>
+                                <td x-text="author.birth_date ? author.birth_date.substring(0,10) : ''"></td>
+                                <td class="text-center">
+                                    <a class="btn btn-sm btn-primary" :href="`/authors/${author.id}/edit`">
+                                        <span class="bi bi-pencil-square"></span>
+                                    </a>
+                                    <form :action="`/authors/${author.id}`" method="POST" style="display:inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger"
+                                            onclick="return confirm('{{ __('Are you sure you want to delete this author?') }}')">
+                                            <span class="bi bi-trash"></span>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </template>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
         <div>{{ $authors->links('pagination::bootstrap-5-limited') }}</div>
     </div>
 @endsection
